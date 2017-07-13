930ac26d48e193c2c74983e5aaaa62cf78f68d91
Jamie <jamie.ellesmere@gmail.com>
2017-07-13 18:47:57 +0100

    Added player creation ui / file saving

--- CatanGamePhone/src/main/java/com/catangame/core/CatanConfiguration.java
+++ CatanGamePhone/src/main/java/com/catangame/core/CatanConfiguration.java
@@ -1,21 +1,27 @@
 package com.catangame.core;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.catangame.game.Player;
 
-import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.stage.Window;
 
 public class CatanConfiguration {
 
 	private static final Logger LOG = LogManager.getLogger(CatanConfiguration.class);
 
-	private static final String PLAYER_CONFIG_FILE = "%APPDATA%/local/CatanGame/player.props";
+	private static final String APP_DATA_LOCATION = getLocalDir();
+	private static final String PLAYER_CONFIG_FILE = APP_DATA_LOCATION + "/CatanGame/player.props";
 
 	private static CatanConfiguration configuration;
 
@@ -26,43 +32,76 @@ public class CatanConfiguration {
 		return configuration;
 	}
 
-	public Optional<Player> loadPlayerDetails(Stage parentWindow) {
-		File f = new File(PLAYER_CONFIG_FILE);
-		if (f.canRead()) {
-			LOG.error("");
-			System.err.println("File can be read");
+	private static String getLocalDir() {
+		String os = System.getenv().get("OS");
+		if (os.toUpperCase().contains("WIN")) {
+			return System.getenv("APPDATA") + "\\..\\Local";
 		} else {
-			if (createFile(f)) {
-				Optional<Player> player = getPlayerUI(parentWindow);
-				if (player.isPresent()) {
-					writePlayer(player.get(), f);	
-				}					
-				return player;
-			} else {
-				return Optional.empty();
-			}			
+			return System.getenv("TEMP");
 		}
-		return Optional.empty();
+	}
+
+	public Optional<Player> loadPlayerDetails(Window window) {
+		File f = new File(PLAYER_CONFIG_FILE);
+		if (f.canWrite()) {
+			return loadPlayerInfo(f);
+		} else {
+			return createAndSave(window, f);
+		}
+	}
+
+	private Optional<Player> loadPlayerInfo(File f) {
+		Properties props = new Properties();
+		try {
+			props.load(new FileInputStream(f));
+			String name = (String) props.get("playerName");
+			String colorString = (String) props.get("playerColour");
+			Color color = Color.valueOf(colorString);
+			return Optional.of(new Player(0, name, color));
+		} catch (IOException e) {
+			LOG.error("Failed to write to file: " + f, e);
+			return Optional.empty();
+		}
+	}
+
+	private Optional<Player> createAndSave(Window window, File f) {
+		if (createFile(f)) {
+			Optional<Player> player = getPlayerUI(window);
+			if (player.isPresent()) {
+				writePlayer(player.get(), f);
+			}
+			return player;
+		} else {
+			return Optional.empty();
+		}
 	}
 
 	private boolean createFile(File f) {
 		try {
-			f.mkdirs();
-			return f.createNewFile();
+			LOG.info("Make dirs: " + f.getParentFile().mkdirs());
+			LOG.info("Make file: " + f.createNewFile());
+			return f.canWrite();
 		} catch (IOException e) {
 			LOG.error("Failed to create file: " + f, e);
 			return false;
 		}
 	}
 
-	private Optional<Player> getPlayerUI(Stage parentWindow) {
-		PlayerEntryDialog playerEntryDialog = new PlayerEntryDialog(parentWindow);		
+	private Optional<Player> getPlayerUI(Window parentWindow) {
+		PlayerEntryDialog playerEntryDialog = new PlayerEntryDialog(parentWindow);
 		playerEntryDialog.showAndWait();
 		return playerEntryDialog.getPlayer();
 	}
 
 	private void writePlayer(Player player, File f) {
-		// TODO Auto-generated method stub
+		Properties props = new Properties();
+		props.put("playerName", player.getName());
+		props.put("playerColour", player.getColor().toString());
 
+		try (FileOutputStream fos = new FileOutputStream(f)) {
+			props.store(fos, "Saved on: " + LocalDateTime.now().toString());
+		} catch (IOException e) {
+			LOG.error("Failed to write to file: " + f, e);
+		}
 	}
 }
--- CatanGamePhone/src/main/java/com/catangame/core/PlayerEntryDialog.java
+++ CatanGamePhone/src/main/java/com/catangame/core/PlayerEntryDialog.java
@@ -3,33 +3,117 @@ package com.catangame.core;
 import java.util.Optional;
 
 import com.catangame.game.Player;
+import com.catangame.util.FXUtils;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.paint.Color;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class PlayerEntryDialog extends Stage {
 
-	private Stage parentWindow;
+	private Window parentWindow;
 	private AnchorPane rootPane;
+	
+	private TextField playerNameField;
+	private ComboBox<Color> colourComboBox;
+	private Player player;
 
-	public PlayerEntryDialog(Stage parentWindow) {
+	public PlayerEntryDialog(Window parentWindow) {
 		super();
 		this.parentWindow = parentWindow;
-		initialiseFX();		
-	}
-	
-	private void initialiseFX() {
-		// TODO Auto-generated method stub
-		
+		initialiseFX();
 	}
 
+	private void initialiseFX() {
+		rootPane = new AnchorPane();
+		setScene(new Scene(rootPane));
+		GridPane gp = initialiseGridPane();
+		
+		Label playerNameLabel = new Label("Name: ");
+		Label colourLabel = new Label("Preferred Color: ");
+		
+		playerNameField = new TextField();
+		playerNameField.setPromptText("Enter name here...");
+		
+		colourComboBox = new ComboBox<>(getColours());
+		
+		Button acceptButton = new Button("Accept");
+		acceptButton.setOnAction(this::onAccept);
+		
+		// row 1
+		gp.add(playerNameLabel, 0, 1);
+		gp.add(playerNameField, 1, 1);
+		
+		// row 2
+		gp.add(colourLabel, 0, 2);
+		gp.add(colourComboBox, 1, 2);
+		
+		// row 3
+		gp.add(acceptButton, 1, 3);
+	}
+
+	private ObservableList<Color> getColours() {
+		ObservableList<Color> list = FXCollections.observableArrayList();
+		
+		list.addAll(Color.RED, Color.BLUE, Color.GREEN);
+		
+		return list;
+	}
+
+	private GridPane initialiseGridPane() {
+		GridPane gp = new GridPane();
+		FXUtils.setAllAnchors(gp, 10.0);
+		rootPane.getChildren().add(gp);
+
+		RowConstraints rc1 = new RowConstraints();
+		rc1.setVgrow(Priority.NEVER);
+		RowConstraints rc2 = new RowConstraints();
+		rc2.setVgrow(Priority.NEVER);
+		RowConstraints rc3 = new RowConstraints();
+		rc3.setVgrow(Priority.NEVER);
+		RowConstraints rc4 = new RowConstraints();
+		rc4.setVgrow(Priority.NEVER);
+		
+		ColumnConstraints cc1 = new ColumnConstraints();
+		cc1.setHgrow(Priority.NEVER);
+		ColumnConstraints cc2 = new ColumnConstraints();
+		cc2.setHgrow(Priority.NEVER);
+
+		return gp;
+	}
+
+	private void onAccept(ActionEvent event) {
+		String playerName = playerNameField.getText();
+		Color playerColour = colourComboBox.getValue();
+		
+		if (playerColour != null & !playerName.isEmpty()) {
+			player = new Player(0, playerName, playerColour);
+		}
+		event.consume();
+	}
+	
 	@Override
 	public void showAndWait() {
-		
+		this.initOwner(parentWindow);
+		this.initModality(Modality.APPLICATION_MODAL);
+		super.showAndWait();
 	}
 
 	public Optional<Player> getPlayer() {
-		// TODO Auto-generated method stub
-		return null;
+		return Optional.of(player);
 	}
 }
